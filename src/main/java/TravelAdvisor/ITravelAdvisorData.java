b06777e51f9bd4a7e11b9ef615c3d4704e88ed7e
BHULKU\sheet <sheetalkailask66@gmail.com>
2024-11-07 17:05:32 -0600

    comment interface method

--- src/main/java/TravelAdvisor/ITravelAdvisorData.java
+++ src/main/java/TravelAdvisor/ITravelAdvisorData.java
@@ -5,35 +5,34 @@ import java.util.ArrayList;
 public interface ITravelAdvisorData {
 
 	//user
-	User getUser(String loginid,String psw);
-	boolean isUserExists(String registerid);
-	void createUser(User u);
+	//User getUser(String loginid,String psw);
+	//boolean isUserExists(String registerid);
+	//void createUser(User u);
 	
 	
 	//attraction
-	void createAttraction(Attraction a);
-	Attraction getAttraction(String attName);
-	ArrayList<Attraction> getAttractions(String torc,String torcvalue);
-	
-	void updateAttraction(Attraction a);
+	//void createAttraction(Attraction a);
+	//Attraction getAttraction(String attName);
+	//ArrayList<Attraction> getAttractions(String torc,String torcvalue);
+	//void updateAttraction(Attraction a);
 	
 	
 	//review
-	void createReview(Review r);
-	ArrayList<Review> getReviews(String attName);
+	//void createReview(Review r);
+	//ArrayList<Review> getReviews(String attName);
 	
 	
 	//favorite
-	ArrayList<Attraction> getFavAttractions(String userid);
+	//ArrayList<Attraction> getFavAttractions(String userid);
 	
 	
 	//qanda
-	ArrayList<String> getQuestions(String attName);
-	void createAns(qanda qa);
-	ArrayList<qanda> viewallqanda(String attName);
+	//ArrayList<String> getQuestions(String attName);
+	//void createAns(qanda qa);
+	//ArrayList<qanda> viewallqanda(String attName);
 	
 	
 	//notification
-	void updateNotification(Notification n);
-	int getNotificationCount(int qid);
+	//void updateNotification(Notification n);
+	//int getNotificationCount(int qid);
 }
